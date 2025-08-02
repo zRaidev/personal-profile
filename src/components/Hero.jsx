0acb2011a9f4daa366c164cf794d11958014e0ce
Raidev <isaac14e2007@gmail.com>
2025-08-01 23:02:26 -0500

    picture styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero ({ targetBlank, textColor }) {
     <header className='relative mb-6'>
       <div className='relative'>
         <div className='absolute top-0 w-screen h-44 bg-gradient-to-b from-[#c2ab8e7b] via-transparent to-transparent' />
-        <img src='/images/profile.webp' alt='profile image' className='w-screen max-h-[55vh] object-cover object-bottom' />
+        <img src='/images/profile.webp' alt='profile image' className='w-screen max-h-[55vh] object-cover object-top' />
         <div className='absolute -bottom-0.5 w-screen h-44 bg-gradient-to-t from-[#c2ab8e] to-transparent' />
       </div>
       <div className='w-full flex justify-center absolute bottom-3'>
